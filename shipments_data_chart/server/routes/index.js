57b9a2e4ee33b6011e461225774f9d681ecf348d
Aparnaa Pazhanivelan <aparnaapazhanivelan68@gmail.com>
2019-06-28 05:56:45 +0530

    modified

--- shipments_data_chart/server/routes/index.js
+++ shipments_data_chart/server/routes/index.js
@@ -8,32 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/upload', (req, res, next) => {
-// console.log(req);
-  let uploadedFile = req.files.file;
-
-  uploadedFile.mv(`${__dirname}/../public/uploadedFiles/${req.body.filename}.json`, err => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send(err);
-    }
-
-    res.json({ file: `public/uploadedFiles/${req.body.filename}.json` });
-    console.log(res.json);
-  });
-});
-
 router.post('/shipmentsData', shipmentsDataController.createMultiple);
 router.post('/vizConfig', vizConfigController.createOne);
 router.get('/shipmentsData', shipmentsDataController.getAll);
 router.get('/vizConfig/:name', vizConfigController.getOne);
-
-router.get('/getShipmentsData', (req, res, next) => {
-  res.json({ file: 'public/uploadedFiles/ShipmentData.csv' });
-})
-
-router.get('/getJsonData', (req, res, next) => {
-  res.json({ file: 'public/uploadedFiles/JsonData.json'});
-})
 
 module.exports = router;
